4953a1299a5f9228a67975ab7daf0e867e74bcdb
leemlwando <leemlwando@gmail.com>
2024-04-21 00:42:49 +0200

    added base url env

--- src/configs.ts
+++ src/configs.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://production.olympusmedia.co.zm/api/v1';
+export const BASE_URL = process.env.BASE_URL ?? 'https://production.olympusmedia.co.zm/api/v1';
 export const SEND_SMS_API = `${BASE_URL}/notify/channels/messages/compose?error_context=CONTEXT_API_ERROR_JSON`;
 export const GET_ACCESS_TOKEN_API = `${BASE_URL}/authentication/gamebox/mobile/signin?error_context=CONTEXT_API_ERROR_JSON`;
 export const GET_SMS_BALANCE_API = `${BASE_URL}/notify/sms-bundles/balance/fetch?error_context=CONTEXT_API_ERROR_JSON`;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { config } from 'dotenv';
+config();
 export * as CONSTANTS from './constants';
 export * as TEST from './testMe';
 export * from './sms';
